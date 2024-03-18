3f31111499a3ef7ef9f44100f225887813d554f2
Chris Vicary <chris_vicary@harvard.edu>
2024-03-18 11:15:57 -0400

    Log LOG_FILE_BACKUP_COUNT config value

--- drs2/__init__.py
+++ drs2/__init__.py
@@ -35,3 +35,5 @@ def configure_logger():  # pragma: no cover
         logger.addHandler(file_handler)
 
     logger.setLevel(log_level)
+    # log the LOG_FILE_BACKUP_COUNT configutration
+    logger.info(f"LOG_FILE_BACKUP_COUNT: {LOG_FILE_BACKUP_COUNT}")
